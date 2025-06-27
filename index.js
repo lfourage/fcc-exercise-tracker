3b1c7947aa0c30b195d555a9a97a7a5b4e848a3a
lfourage <ludogriph@gmail.com>
2025-06-27 10:53:49 +0200

    added /logs response

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ mongoose.connect(process.env.MONGO_URI);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const exerciseSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: String, required: false },
   _id: { type: mongoose.Types.ObjectId, required: true },
+  username: { type: String, required: true },
+  date: { type: String, required: false },
+  duration: { type: Number, required: true },
+  description: { type: String, required: true },
 });
 
 const userSchema = new mongoose.Schema({
@@ -58,28 +58,74 @@ app.post("/api/users", (req, res) => {
         username: username,
       });
       newUser.save();
-      res.json(newUser);
+
+      const newLog = new log({
+        username: username,
+        count: 0,
+        _id: newUser._id,
+        log: [],
+      });
+      newLog.save();
+
+      res.json({
+        id: newUser._id,
+        username: username,
+      });
     }
   });
 });
 
-app.post('/api/users/:_id/exercises', (req, res) => {
-  const id = req.params._id;
-  const description = req.params.description;//not working
-  const duration = req.params.duration;//not working
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const _id = req.params._id;
+  const date = new Date(req.body.date).toDateString();
+  const duration = req.body.duration;
+  const description = req.body.description;
 
-  user.findById(id).then((data) => {
+  user.findById(_id).then((data) => {
     if (data) {
       const newExercise = new exercise({
+        _id: _id,
         username: data.username,
+        date: date,
         description: description,
         duration: duration,
-        _id: id,
       });
-      res.json(newExercise);
+      newExercise.save();
+
+      log.findByIdAndUpdate(_id, {
+        $inc: { count: 1 },
+        $push: {
+          log: {
+            description: description,
+            duration: duration,
+            date: date,
+          },
+        },
+      });
+
+      res.json({
+        _id: _id,
+        username: data.username,
+        date: date,
+        description: description,
+        duration: duration,
+      });
     }
-  })
-})
+  });
+});
+
+app.get("/api/users/:_id/logs", (req, res) => {
+  const _id = req.params._id;
+
+  log.findById(_id).then((data) => {
+    res.json({
+      _id: _id,
+      username: data.username,
+      count: data.count,
+      log: data.log,
+    });
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
